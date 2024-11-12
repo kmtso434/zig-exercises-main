f7792d3cdcb12cc52229991803fb4a5778c2b813
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2024-11-12 15:51:51 +0900

    first commit

---
+++ exercises/exercises/030_switch.zig
@@ -0,0 +1,40 @@
+//
+// The "switch" statement lets you match the possible values of an
+// expression and perform a different action for each.
+//
+// This switch:
+//
+//     switch (players) {
+//         1 => startOnePlayerGame(),
+//         2 => startTwoPlayerGame(),
+//         else => {
+//             alert();
+//             return GameError.TooManyPlayers;
+//         }
+//     }
+//
+// Is equivalent to this if/else:
+//
+//     if (players == 1) startOnePlayerGame();
+//     else if (players == 2) startTwoPlayerGame();
+//     else {
+//         alert();
+//         return GameError.TooManyPlayers;
+//     }
+//
+const std = @import("std");
+
+pub fn main() void {
+    const lang_chars = [_]u8{ 26, 9, 7, 42 };
+
+    for (lang_chars) |c| {
+        switch (c) {
+            7 => std.debug.print("G", .{}),
+            9 => std.debug.print("I", .{}),
+            26 => std.debug.print("Z", .{}),
+            else => std.debug.print("?", .{}),
+        }
+    }
+
+    std.debug.print("\n", .{});
+}
---
+++ exercises/exercises/034_quiz4.zig
@@ -0,0 +1,27 @@
+//
+// Quiz time. See if you can make this program work!
+//
+// Solve this any way you like, just be sure the output is:
+//
+//     my_num=42
+//
+const std = @import("std");
+
+const NumError = error{IllegalNumber};
+
+pub fn main() void {
+    const stdout = std.io.getStdOut().writer();
+
+    const my_num: u32 = try getNumber(42);
+
+    try stdout.print("my_num={}\n", .{my_num});
+}
+
+// This function is obviously weird and non-functional. But you will not be changing it for this quiz.
+fn getNumber(n: u32) NumError!u32 {
+    if (n != 42) {
+        return NumError.IllegalNumber;
+    } else {
+        return 42;
+    }
+}
